5651ec226004281ee5c4b45db18a9cfa473259a8
FKgk <rhkd865@gmail.com>
2020-03-07 17:11:42 +0900

    add setup

---
+++ setup.py
@@ -0,0 +1,18 @@
+import setuptools
+
+setuptools.setup(
+	name="smail",
+	version="1.0",
+	license="MIT",
+	author="Kim Gwangho",
+	author_email="rhkd865@gmail.com",
+	description="",
+	long_description=open('README.md').read(),
+	url='https://github.com/FKgk/smail',
+	packages=setuptools.find_packages(),
+	classifiers=[
+		"Programming Language :: Python :: 3",
+		"License :: OSI Approved :: MIT License",
+		"Operationg System :: OS Independent"
+	]
+)
